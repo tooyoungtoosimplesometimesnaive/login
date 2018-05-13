771aed35a214dc913d8189f2337cd43344109d62
Xing <xingmarc@yeah.net>
2018-05-12 17:48:30 -0700

    First implementation.

--- index.js
+++ index.js
@@ -2,19 +2,68 @@
 var usernameInput = document.getElementById('username');
 var passwordInput = document.getElementById('password');
 
+var leftEye = document.getElementById('left-eye');
+var rightEye = document.getElementById('right-eye');
+
+var leftPaw = document.getElementById('left-paw');
+var rightPaw = document.getElementById('right-paw');
+
+var LENGTH = 24
+
+function rotateEyes(username) {
+	if (!username)
+		return
+
+	var inputLen = username.length;
+
+	var deg = inputLen / LENGTH * 90;
+	leftEye.classList.remove("eye-on-focus");
+	leftEye.style.transformOrigin = "100px 200px";
+	leftEye.style.transform = 'rotate(-' + deg + 'deg) translate(-20px, 20px)';
+	rightEye.classList.remove("eye-on-focus");
+	rightEye.style.transformOrigin = "200px 200px";
+	rightEye.style.transform = 'rotate(-' + deg + 'deg) translate(-20px, 20px)';
+}
+
 usernameInput.oninput = function(Event) {
-	console.log(usernameInput.value)
+	rotateEyes(usernameInput.value)
 }
 
 usernameInput.onfocus = function() {
-	console.log("username focused");
+	if (usernameInput.value) {
+
+		console.log('something')
+		rotateEyes(usernameInput.value)
+	}
+	else {
+		leftEye.classList.add("eye-on-focus");
+		rightEye.classList.add("eye-on-focus");
+	}
+}
+
+usernameInput.onblur = function() {
+	leftEye.classList.remove("eye-on-focus");
+	rightEye.classList.remove("eye-on-focus");
+	leftEye.style.transform = 'none';
+	rightEye.style.transform = 'none';
 }
 
 passwordInput.onfocus = function() {
-	console.log("password focused");
+	leftPaw.classList.remove('left-paw');
+	leftPaw.classList.add('left-paw-up');
+	leftPaw.classList.remove('left-paw-down');
+	rightPaw.classList.remove('right-paw');
+	rightPaw.classList.add('right-paw-up');
+	rightPaw.classList.remove('right-paw-down');
 }
 
 passwordInput.onblur = function() {
-	console.log("password blurred");
+	leftPaw.classList.remove('left-paw');
+	leftPaw.classList.remove('left-paw-up');
+	leftPaw.classList.add('left-paw-down');
+	rightPaw.classList.remove('right-paw');
+	rightPaw.classList.remove('right-paw-up');
+	rightPaw.classList.add('right-paw-down');
+
 }
 
